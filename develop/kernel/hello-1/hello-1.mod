6d8794ae2c3a3cf7557ef7d2f94feee2cfabb686
Sam kleiman <sdivcom@yandex.ru>
2022-10-07 14:59:21 +0300

    added rm.sh

--- develop/kernel/hello-1/hello-1.mod
+++
@@ -1,2 +0,0 @@
-/home/sam/Documents/linux-kernel-module-lessons/develop/kernel/hello-1/hello-1.o
-
---
+++ develop/kernel/hello-1/rm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm hello-1.ko
+rm hello-1.mod
+rm hello-1.mod.c
+rm hello-1.mod.o
+rm hello-1.o
+rm modules.order
+rm Module.symvers
